509f6f85ec97ff0b0febf6e900b6b9b79e49393b
George Milner <44804443+gmilner@users.noreply.github.com>
2019-10-30 15:41:21 +0000

    Completed exercise 1

--- public/agreement.php
+++ public/agreement.php
@@ -1,6 +1,8 @@
 <?php
 include_once 'header.php';
 include_once 'footer.php';
+
+
 ?>
 
 <body class="bg-info">
@@ -9,11 +11,9 @@ include_once 'footer.php';
         <div class="card mt-3 px-2 py-2">
             <h1>Stationery Application</h1>
             <p>Welcome to this fictional stationery application page created for ISAD251 example. To continue
-            please accept our terms and conditions</p>
-            <form action="agreement.php" method = "post">
-                <input type="radio" name="tacs" value="agree"> Agree<br>
-                <input type="radio" name="tacs" value="disagree"> Disagree<br>
-                <input type="submit"><br>
+                please accept our terms and conditions</p>
+            <form>
+            <?php echo "Welcome. You have selected $_POST[tacs] for the agreement" ?>
             </form>
         </div>
     </div>
