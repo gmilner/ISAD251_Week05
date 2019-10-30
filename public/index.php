3f071cffba9a5113cd7c112348c6662b3f77a2a9
George Milner <44804443+gmilner@users.noreply.github.com>
2019-10-30 13:22:08 +0000

    Barely started

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'public/header.php';
+include_once 'public/footer.php';
+?>
+
+<body class="bg-info">
+<div class="container-fluid col-md-10 offset-md-1">
+    <div class="row">
+        <div class="card mt-3 px-2 py-2">
+            <h1>Stationery Application</h1>
+            <p>Welcome to this fictional stationery application page created for ISAD251 example. To continue
+            please accept our terms and conditions</p>
+        </div>
+    </div>
+</div>
+</body>
